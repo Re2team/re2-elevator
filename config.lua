51b0364e6a2e2080868c7405c35d3f5e8cdf42ba
TRClassic <82931913+trclassic92@users.noreply.github.com>
2022-06-01 12:06:10 -0400

    Typo in config
    I had to make a PR for them typos

--- config.lua
+++ config.lua
@@ -1,19 +1,19 @@
 Config = {}
 
 Config.locations = {
-    ["Pilbox"] = {
+    ["Pillbox"] = {
         [1] = {
-            ["name"] = "Philbox RoofTop",
+            ["name"] = "Pillbox RoofTop",
             ["teleport"] = vector4(338.53, -583.9, 74.16, 252.7),
             ["button"] = vector4(338.53, -583.9, 74.16, 252.7)
         },
         [2] = {
-            ["name"] = "Philbox First Floor",
+            ["name"] = "Pillbox First Floor",
             ["teleport"] = vector4(332.39, -595.77, 43.28, 70.94),
             ["button"] = vector4(331.74, -596.9, 43.28, 245.58)
         },
         [3] = {
-            ["name"] = "Philbox Parking",
+            ["name"] = "Pillbox Parking",
             ["teleport"] = vector4(344.28, -586.2, 28.8, 244.59),
             ["button"] = vector4(345.05, -584.85, 28.8, 73.66)
         },
